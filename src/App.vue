667e8f2ce1551b03d1927defa8fc6122b5485a02
dark850916 <dark850916@gmail.com>
2024-12-18 11:48:51 -0500

    Creadas todas las tarjetas y Lista de trabajos.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import Navbar from "./components/Navbar.vue";
 import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
+import Alljobs from "@/components/Alljobs.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <HomeCards />
+  <JobListings :limit="3" />
+  <Alljobs />
 </template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,29 @@
+<script setup>
+import JobCard from "@/components/JobCard.vue";
+import JobData from "@/jobs.json";
+import { ref } from "vue";
+const jobs = ref(JobData);
+
+defineProps({
+  limit: {
+    type: Number,
+  },
+});
+</script>
+<template>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Buscar trabajos
+      </h2>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <JobCard
+        v-for="job in jobs.slice(0, limit || jobs.length)"
+        :key="job.id"
+        :job="job"
+      />
+    </div>
+    <!-- Aqui va el componente de JobCard  -->
+  </section>
+</template>
